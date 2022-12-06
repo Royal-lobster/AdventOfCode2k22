2f7b1fb5f7210e28726f3b424ab97e368dcecadf
Srujan Gurram <srujangs8@gmail.com>
2022-12-06 16:07:57 +0530

    finished day 6 ✨

---
+++ src/day-6/a.ts
@@ -0,0 +1,16 @@
+const input = require("fs").readFileSync("./input.txt", "utf-8") as string;
+
+let charCountForMarker = 0;
+let slidingWindow = [];
+const WINDOW_SIZE = 4;
+
+input.split("").every((char, i) => {
+  if (slidingWindow.length === WINDOW_SIZE) {
+    if (new Set(slidingWindow).size === WINDOW_SIZE)
+      return !(charCountForMarker = i);
+    slidingWindow.shift();
+  }
+  return slidingWindow.push(char);
+});
+
+console.log(`Count till marker character: ${charCountForMarker}`);
